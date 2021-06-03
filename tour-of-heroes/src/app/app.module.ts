61a0041e1d27c13052affc43928e488420140a1f
ArtyomBlr <artem.zborovsky2@gmail.com>
2021-06-03 12:06:55 +0300

    STEP5: used httpClient (get, post, put, delet)

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,9 +19,10 @@ import { DashboardsComponent } from './dashboards/dashboards.component';
     DashboardsComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
